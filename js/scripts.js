480f9b85057f03cae4c9e1a7877470f90f8f577c
Lulu <lulumutuli24@gmail.com>
2018-02-14 12:58:43 +0300

    initial javascript scrippt made

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".showing").toggle();
+
+  });
+});
+$(document).ready(function() {
+$(".clickable").click(function() {
+  $(".operators").toggle();
+
+  });
+});
